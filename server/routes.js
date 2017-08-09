d2b2acbae0fce7e468426a505b508d342e392e1c
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-09 15:21:45 -0700

    Add index.html

--- server/routes.js
+++ server/routes.js
@@ -2,11 +2,12 @@
 var express = require('express');
 
 var app = express();
+app.use(express.static(__dirname + '/../client'));
+
 
 app.get('/', function (req, res) {
   res.send('Hello world');
 });
-
 
 
 module.exports = app;
