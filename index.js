4963bc4a9386e1908a6eeb5dd1296e42d819676d
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-09 14:51:12 -0700

    Basic server

---
+++ index.js
@@ -0,0 +1,7 @@
+var server = require('./server/routes.js');
+var port = 8000;
+
+
+
+server.listen(port);
+console.log('Now listening on local host port:' + port);
---
+++ server/routes.js
@@ -0,0 +1,12 @@
+//var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello world');
+});
+
+
+
+module.exports = app;
