dcaf33874e2322281ded7df7316e605a9f019f04
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-14 13:56:52 -0700

    Add notes in index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var mysql = require('./db/config.js')
 var port = process.env.PORT || 8000;
 
+//this part only runs after config.js runs
 mysql.createDatabase()
   .then(function() {
+    //boot up the rest of the backend
     var server = require('./server/routes.js');
+
+    //server is now listening
     server.listen(port);
     console.log('Listening on port ', port);
   })
