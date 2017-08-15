38e3496fb673b5a9b3cb2d48d899b96ca90846c5
Sonrisa <sonrisa722611@gmail.com>
2017-08-15 12:52:36 -0700

    Add upload componenet

---
+++ client/components/uploadRecipe/uploadRecipe.js
@@ -0,0 +1,11 @@
+angular.module('app')
+
+.controller ('UploadRecipeCtrl', function ($scope) {
+
+})
+
+.component('uploadRecipe', {
+  controller: 'UploadRecipeCtrl',
+  templateUrl: 'client/components/uploadRecipe/uploadRecipe.html',
+
+})
