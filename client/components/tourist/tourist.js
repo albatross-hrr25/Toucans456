a0937d64fc682734b2e5b232962c770000abf728
Christine Zimmerman <ChristineOrtega@gmail.com>
2017-08-16 15:52:49 -0700

    Tourist component renders its html template

--- client/components/tourist/tourist.js
+++ client/components/tourist/tourist.js
@@ -1,23 +1,30 @@
-console.log('TOURIST');
-
 angular.module('app') // ADD $NGROUTER HERE OR IN APP.JS?
 
   .config(function($stateProvider) {
-    console.log('inside TOURIST');
-    //$urlRouterProvider.otherwise('/');
 
     var touristState = {
       name: 'tourist',
       url: '/tourist',
-      templateUrl: 'client/components/tourist/tourist.html'
-    }
+      component: 'tourist'
+      // Resolve block will be needed if we do any routing for touristState
+      // resolve: {
+      //   tourist: function(touristService) {
+      //     // delegate to services which return a promise
+      //     //
+      //   }
+      // }
+    };
 
     $stateProvider.state(touristState);
-  });
+  })
   // .controller('TouristCtrl', function($scope) {
 
   // })
-  // .component('tourist', {
-  //   controller: 'TouristCtrl',
-  //   //templateUrl: 'client/components/tourist/tourist.html',
-  // });
+  .component('tourist', {
+    bindings: {
+      tourist: '<'
+    },
+    //controller: 'TouristCtrl',
+    templateUrl: 'client/components/tourist/tourist.html'
+
+  });
