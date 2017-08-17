512491d91df9197aae09944e795ea957672aa68d
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-17 10:24:08 -0700

    Before floobits

--- client/components/app/app.js
+++ client/components/app/app.js
@@ -15,14 +15,52 @@ angular.module('app')
   };
 
 ////////search bar//////////////////////////
+
+// this.handleSearchResults = (query) => {
+//   var results = $scope.recipes.filter((recipe) => {
+//     var pattern = ".*" + query.toString() + ".*";
+//     var re = new RegExp(pattern, "i");
+//     return recipe.title.toString().match(re) ? true : false;
+//   })
+//   $scope.recipes = results;
+// };
   this.handleSearchResults = (query) => {
+  // for each recipe
+    // check if the title matches the regex
+    // get the tags for this recipe
+      // check if any of the tags match the regex
+
+
     var results = $scope.recipes.filter((recipe) => {
-      var pattern = ".*" + query.toString() + ".*";
-      var re = new RegExp(pattern, "i");
-      return recipe.title.toString().match(re) ? true : false;
+    var pattern = ".*" + query.toString() + ".*";
+    var re = new RegExp(pattern, "i");
+    var isTitleMatch = recipe.title.toString().match(re)
+    var isMatch = false;
+
+    get.getTags({id: recipe.id}, function(tags) {
+      tags.forEach(function(tag) {
+        if(tag.tag.toString().match(re) || isTitleMatch) {
+          console.log("true!!!")
+          isMatch = true;
+        }
+      })
+      console.log("RETURN isMatch:", isMatch)
+      //return isMatch;
     })
+
+    console.log("GET TAGS THEN", get.getTags.then);
+    // console.log("RETURN isMatch to filter:", isMatch)
+    return isMatch;
+
+      //return isTagMatch || recipe.title.toString().match(re) ? true : false;
+    })
+
+    console.log("results: ", results)
     $scope.recipes = results;
   };
+
+
+
 
   this.handleClickHome = () => {
     this.content = true;
@@ -57,5 +95,8 @@ angular.module('app')
     $scope.primaryRecipe = recipes[0];
     $scope.$apply();
   });
+
+
+
 
 });
