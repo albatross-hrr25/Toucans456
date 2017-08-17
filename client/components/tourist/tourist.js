3a802880e2e23d9bfe1ab8a48127fbfecac64ac6
Christine Zimmerman <ChristineOrtega@gmail.com>
2017-08-17 11:26:10 -0700

    Tourist controller stubbed out

--- client/components/tourist/tourist.js
+++ client/components/tourist/tourist.js
@@ -1,6 +1,11 @@
 angular.module('app')
+  .controller('TouristCtrl', function($scope, get) {
+    get.login = () => {
+      // utilizes a service
+    };
+  })
   .component('tourist', {
-
+    controller: 'TouristCtrl',
     templateUrl: 'client/components/tourist/tourist.html'
 
   });
