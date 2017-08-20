f5f9fe6fbe60acb14bde03fcb9cab01b1399389e
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-20 15:34:26 -0700

    Ready for rebase

--- client/config.js
+++ client/config.js
@@ -14,6 +14,33 @@ angular.module('app', ['ui.router'])
       component: 'app'
     };
 
+    //TESTING//
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'views/home.html',
+      resolve: {
+        user: function($auth) {
+          return $auth.getUser();
+        }
+      }
+    });
+    //to allow access to a view to go through or not
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'views/home.html',
+      resolve: {
+        user: function($auth) {
+          return $auth.getUser()
+            .then(function(user) {
+              //can access resource?
+              //return true/false
+            })
+        }
+      }
+    })
+
+    //TESTING//
+
     $stateProvider.state(touristState);
     $stateProvider.state(primaryState);
 
