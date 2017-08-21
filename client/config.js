ccacb5c63431041a1e6eb67ae4c57b63af7c119a
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-20 20:14:08 -0700

    Upload recipe page update

--- client/config.js
+++ client/config.js
@@ -12,34 +12,11 @@ angular.module('app', ['ui.router'])
       name: 'primary',
       url: '/primary',
       component: 'app'
+      // resolve: {
+      //
+      // }
     };
 
-    //TESTING//
-    // $stateProvider.state('home', {
-    //   url: '/',
-    //   templateUrl: 'views/home.html',
-    //   resolve: {
-    //     user: function($auth) {
-    //       return $auth.getUser();
-    //     }
-    //   }
-    // });
-    // //to allow access to a view to go through or not
-    // $stateProvider.state('home', {
-    //   url: '/',
-    //   templateUrl: 'views/home.html',
-    //   resolve: {
-    //     user: function($auth) {
-    //       return $auth.getUser()
-    //         .then(function(user) {
-    //           //can access resource?
-    //           //return true/false
-    //         })
-    //     }
-    //   }
-    // })
-
-    //TESTING//
 
     $stateProvider.state(touristState);
     $stateProvider.state(primaryState);
