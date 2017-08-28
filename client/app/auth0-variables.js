08b37d136be894441a41e36a73482d12917e4bfc
Lisa Gee <ZhuSuFeng@gmail.com>
2017-08-27 18:44:36 -0700

    Change auth0_client_id

--- client/app/auth0-variables.js
+++ client/app/auth0-variables.js
@@ -1,4 +1,4 @@
-var AUTH0_CLIENT_ID = 'Zak43COwtpgU3Ei9vDiL8BX7fQc90iRx';
+var AUTH0_CLIENT_ID = 'aZbdnVihkR6huEZNVWBRFkTb2l5I1Tk5';
 var AUTH0_DOMAIN = 'zhusufeng.auth0.com';
 var AUTH0_CALLBACK_URL = 'http://localhost:8000/';
 var AUTH0_API_AUDIENCE = 'angular';
