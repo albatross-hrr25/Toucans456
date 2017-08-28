5eb5675f718b88f8cdbc695fcae800bf632a9222
Lisa Gee <ZhuSuFeng@gmail.com>
2017-08-27 20:03:10 -0700

    Add more authentication and state for ui-routing

--- client/app/app.js
+++ client/app/app.js
@@ -1,17 +1,20 @@
 angular.module('app', ['auth0.auth0', 'ui.router', 'angular-jwt'])
-  .config(function($stateProvider, $urlRouterProvider, $locationProvider, jwtOptionsProvider, angularAuth0Provider) {
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider, jwtOptionsProvider, angularAuth0Provider, $httpProvider) {
 
     $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: '../views/tourist.html' 
+      template: '<tourist></tourist>', 
     })
     .state('primary', {
       url: '/primary',
-      controller: 'RecipeController',
-      template: '<main></main>'
-      // templateUrl: '../views/main.html' 
+      template: '<main></main>',
+      onEnter: checkAuthentication
     })
+    .state('callback', {
+      url: '/callback',
+      template: '<callback></callback>'
+    });
 
     // Initialization for the angular-auth0 library
     angularAuth0Provider.init({
@@ -20,6 +23,7 @@ angular.module('app', ['auth0.auth0', 'ui.router', 'angular-jwt'])
       responseType: 'token id_token',
       redirectUri: AUTH0_CALLBACK_URL,
       audience: AUTH0_API_AUDIENCE,
+      scope: REQUESTED_SCOPES
     });
     
     // Configure a tokenGetter so that the isAuthenticated
@@ -27,74 +31,37 @@ angular.module('app', ['auth0.auth0', 'ui.router', 'angular-jwt'])
     jwtOptionsProvider.config({
       tokenGetter: function() {
         return localStorage.getItem('id_token');
-      }
+      },
+      whiteListedDomains: ['localhost', '127.0.0.1', 'recipes-archive.herokuapp.com']
     });
+
+    $httpProvider.interceptors.push('jwtInterceptor');
     
     $urlRouterProvider.otherwise('/');
 
     // Remove the ! from the hash so that
     // auth0.js can properly parse it
     $locationProvider.hashPrefix('');
+    
+    // Comment out the line below to run the app
+    // without HTML5 mode (will use hashes in routes)
+    // $locationProvider.html5Mode(true);
 
+    function checkAuthentication($transition$) {
+      var $state = $transition$.router.stateService;
+      var auth = $transition$.injector().get('authService');
+      if (!auth.isAuthenticated()) {
+        return $state.target('home');
+      }
+    }
+
+    function checkForScopes(scopes) {
+      return function checkAuthentication($transition$) {
+        var $state = $transition$.router.stateService;
+        var auth = $transition$.injector().get('authService');
+        if (!auth.isAuthenticated() || !auth.userHasScopes(scopes)) {
+          return $state.target('home');
+        }
+      }
+    }
   });
-
-// ///////// JWT /////////
-//     jwtOptionsProvider.config({
-//       //unauthenticatedRedirectPath: '/login',
-//       tokenGetter: function() {
-//         return localStorage.getItem('id_token');  //where does this come from
-//       },
-//       whiteListedDomains: ['127.0.0.1', 'localhost']
-//     });
-
-//     jwtInterceptorProvider.tokenGetter = function(store) {
-//       return store.get('id_token');
-//     }
-// ///////// REDIRECT /////////
-//     // redirect = function($q, $timeout, store, $location) {
-//     //   $timeout(function() {
-//     //     store.get('id_token');
-//     //     console.log('running timeout');
-//     //   })
-//     //   return {
-//     //     responseError: function(err) {
-//     //       if(err.status === 401) {
-//     //         console.log('There is an error: ', err);
-//     //         $location.path('/tourist');
-//     //       }
-//     //       return $q.reject(err);
-//     //     }
-//     //   }
-//     // }
-
-// ///////// REGISTERING STATES AND FUNCTIONS /////////
-//     $httpProvider.interceptors.push('jwtInterceptor');
-//     //$httpProvider.interceptors.push('redirect');
-
-//     $stateProvider.state(touristState);
-//     $stateProvider.state(primaryState);
-
-//   })
-//   .run(function($rootScope, $state, $location, jwtHelper, store) {
-//     $rootScope.$on('$locationChangeStart', function() {
-//       var token = store.get('id_token');
-
-//       if (token) {  //does this token exist?
-//         if (typeof token === 'object') {
-//           token = token.data;
-//         }
-//         if (jwtHelper.isTokenExpired(token) === false) {  //is it expired?
-//           console.log('Staying at primary');
-//           axios.defaults.headers.common.Authorization = 'Bearer ' + token;
-
-//           $location.path('/primary');  //head to the primary page
-//         } else {  //if it is expired head back to tourist page
-//           console.log('Going home to tourist');
-//           $location.path('/tourist');
-//         }
-//       } else {  //no token
-//         console.log('Going home to tourist');
-//         $location.path('/tourist');
-//       }
-//     });
-//   });
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,15 +1,17 @@
 'use strict';
 
-angular.module('app', ['auth0.auth0', 'ui.router', 'angular-jwt']).config(function ($stateProvider, $urlRouterProvider, $locationProvider, jwtOptionsProvider, angularAuth0Provider) {
+angular.module('app', ['auth0.auth0', 'ui.router', 'angular-jwt']).config(function ($stateProvider, $urlRouterProvider, $locationProvider, jwtOptionsProvider, angularAuth0Provider, $httpProvider) {
 
   $stateProvider.state('home', {
     url: '/',
-    templateUrl: '../views/tourist.html'
+    template: '<tourist></tourist>'
   }).state('primary', {
     url: '/primary',
-    controller: 'RecipeController',
-    template: '<main></main>'
-    // templateUrl: '../views/main.html' 
+    template: '<main></main>',
+    onEnter: checkAuthentication
+  }).state('callback', {
+    url: '/callback',
+    template: '<callback></callback>'
   });
 
   // Initialization for the angular-auth0 library
@@ -18,7 +20,8 @@ angular.module('app', ['auth0.auth0', 'ui.router', 'angular-jwt']).config(functi
     domain: AUTH0_DOMAIN,
     responseType: 'token id_token',
     redirectUri: AUTH0_CALLBACK_URL,
-    audience: AUTH0_API_AUDIENCE
+    audience: AUTH0_API_AUDIENCE,
+    scope: REQUESTED_SCOPES
   });
 
   // Configure a tokenGetter so that the isAuthenticated
@@ -26,76 +29,40 @@ angular.module('app', ['auth0.auth0', 'ui.router', 'angular-jwt']).config(functi
   jwtOptionsProvider.config({
     tokenGetter: function tokenGetter() {
       return localStorage.getItem('id_token');
-    }
+    },
+    whiteListedDomains: ['localhost', '127.0.0.1', 'recipes-archive.herokuapp.com']
   });
+
+  $httpProvider.interceptors.push('jwtInterceptor');
 
   $urlRouterProvider.otherwise('/');
 
   // Remove the ! from the hash so that
   // auth0.js can properly parse it
   $locationProvider.hashPrefix('');
+
+  // Comment out the line below to run the app
+  // without HTML5 mode (will use hashes in routes)
+  // $locationProvider.html5Mode(true);
+
+  function checkAuthentication($transition$) {
+    var $state = $transition$.router.stateService;
+    var auth = $transition$.injector().get('authService');
+    if (!auth.isAuthenticated()) {
+      return $state.target('home');
+    }
+  }
+
+  function checkForScopes(scopes) {
+    return function checkAuthentication($transition$) {
+      var $state = $transition$.router.stateService;
+      var auth = $transition$.injector().get('authService');
+      if (!auth.isAuthenticated() || !auth.userHasScopes(scopes)) {
+        return $state.target('home');
+      }
+    };
+  }
 });
-
-// ///////// JWT /////////
-//     jwtOptionsProvider.config({
-//       //unauthenticatedRedirectPath: '/login',
-//       tokenGetter: function() {
-//         return localStorage.getItem('id_token');  //where does this come from
-//       },
-//       whiteListedDomains: ['127.0.0.1', 'localhost']
-//     });
-
-//     jwtInterceptorProvider.tokenGetter = function(store) {
-//       return store.get('id_token');
-//     }
-// ///////// REDIRECT /////////
-//     // redirect = function($q, $timeout, store, $location) {
-//     //   $timeout(function() {
-//     //     store.get('id_token');
-//     //     console.log('running timeout');
-//     //   })
-//     //   return {
-//     //     responseError: function(err) {
-//     //       if(err.status === 401) {
-//     //         console.log('There is an error: ', err);
-//     //         $location.path('/tourist');
-//     //       }
-//     //       return $q.reject(err);
-//     //     }
-//     //   }
-//     // }
-
-// ///////// REGISTERING STATES AND FUNCTIONS /////////
-//     $httpProvider.interceptors.push('jwtInterceptor');
-//     //$httpProvider.interceptors.push('redirect');
-
-//     $stateProvider.state(touristState);
-//     $stateProvider.state(primaryState);
-
-//   })
-//   .run(function($rootScope, $state, $location, jwtHelper, store) {
-//     $rootScope.$on('$locationChangeStart', function() {
-//       var token = store.get('id_token');
-
-//       if (token) {  //does this token exist?
-//         if (typeof token === 'object') {
-//           token = token.data;
-//         }
-//         if (jwtHelper.isTokenExpired(token) === false) {  //is it expired?
-//           console.log('Staying at primary');
-//           axios.defaults.headers.common.Authorization = 'Bearer ' + token;
-
-//           $location.path('/primary');  //head to the primary page
-//         } else {  //if it is expired head back to tourist page
-//           console.log('Going home to tourist');
-//           $location.path('/tourist');
-//         }
-//       } else {  //no token
-//         console.log('Going home to tourist');
-//         $location.path('/tourist');
-//       }
-//     });
-//   });
 'use strict';
 
 angular.module('app').run(function (authService) {
@@ -107,9 +74,17 @@ angular.module('app').run(function (authService) {
 
 var AUTH0_CLIENT_ID = 'aZbdnVihkR6huEZNVWBRFkTb2l5I1Tk5';
 var AUTH0_DOMAIN = 'zhusufeng.auth0.com';
-var AUTH0_CALLBACK_URL = 'http://localhost:8000/callback';
+var AUTH0_CALLBACK_URL = 'http://localhost:8000/';
 var AUTH0_API_AUDIENCE = 'angular';
 var REQUESTED_SCOPES = 'openid profile read:messages write:messages';
+'use strict';
+
+angular.module('app').component('callback', {
+
+  controller: 'CallbackCtrl',
+  templateUrl: 'views/callback.html'
+
+});
 'use strict';
 
 angular.module('app').component('inventory', {
@@ -198,6 +173,9 @@ angular.module('app').component('uploadRecipe', {
 });
 
 //Checked KK
+'use strict';
+
+angular.module('app').controller('CallbackCtrl', function () {});
 'use strict';
 
 angular.module('app').controller('InventoryCtrl', function () {});
