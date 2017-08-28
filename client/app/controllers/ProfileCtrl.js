eb48483f13381bb89d0f562ca2d5f8a80214e082
kevinlabs <kevinlabx@gmail.com>
2017-08-28 00:15:21 -0500

    -Major update on the front end view  -Updated CSS  -Created new UI-View  -Created and update angular component.  -Updated file structure.

--- client/app/controllers/ProfileCtrl.js
+++ client/app/controllers/ProfileCtrl.js
@@ -1,15 +1,20 @@
 angular.module('app').controller ('ProfileCtrl', function ($scope, authService) {
 
+    $scope.testmekevin = "Kevin is so cool.";
+
     var vm = this;
     vm.auth = authService;
     vm.profile;
 
+    debugger;
     if (authService.getCachedProfile()) {
       vm.profile = authService.getCachedProfile();
+      console.log('Showing Profile: ', vm.profile);
     } else {
       authService.getProfile(function(err, profile) {
         vm.profile = profile;
         $scope.$apply();
+        console.log('Showing Profile: ', vm.profile);
       });
     }
     
